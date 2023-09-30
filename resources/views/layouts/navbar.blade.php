ce2bea1a50e1091ce5995f0a89fcc1c69e3b4334
Al Bukhari Bin Riedho <bukhari0281@gmail.com>
2023-10-01 05:04:39 +0700

    COBA NAMBAH AUTH

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -13,7 +13,7 @@
                 <li class="nav-item">
                     <a class="nav-link {{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
                 </li>
-            @else    
+            @else
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         {{ Auth::user()->name }}
@@ -35,4 +35,4 @@
     </div>
 </nav>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+// use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginRegisterController;
 
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.dashboard');
-});
+Route::get('/', 'HomeController@index' )
+    ->name('home');
 
 Route::controller(LoginRegisterController::class)->group(function() {
     Route::get('/register', 'register')->name('register');
@@ -35,3 +35,7 @@ Route::get('posts/{post}', [PostController::class, 'show'])->name('users.show');
 Route::match(['put', 'patch'], 'users/{user}', [UserController::class, 'update'])->name('users.update');
 Route::delete('posts/{user}', [PostController::class, 'destroy'])->name('users.destroy');
 
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
